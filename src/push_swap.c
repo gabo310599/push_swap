caac4e8f21518d03cce40ac69bd5ab81caf572ce
gabo310599 <gabrielojedasanchez@gmail.com>
2025-06-10 14:18:38 +0200

    create stack structure

--- src/push_swap.c
+++ src/push_swap.c
@@ -6,14 +6,23 @@
 /*   By: gojeda <gojeda@student.42madrid.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/08 17:49:41 by gojeda            #+#    #+#             */
-/*   Updated: 2025/06/08 21:06:37 by gojeda           ###   ########.fr       */
+/*   Updated: 2025/06/10 14:17:44 by gojeda           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft/libft.h"
+#include "../include/push_swap.h"
 
-int main(void)
+int main(int argc, char **argv)
 {
-    ft_printf("HOLA MUNDO\n!");
+    t_stack_node    *a;
+    t_stack_node    *b;
+
+    a = NULL;
+    b = NULL;
+    if (argc == 1 || (argc == 2 && !argv[1][0]))
+        return (1);
+    else if (argc == 2)
+        argv = ft_split(argv[1], ' ');
+    
     return (0);
 }
